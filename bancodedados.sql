4edbfdf2ac7d89439b1b4554c3cc41198a4a5dd9
daniel sena <danielsenasanti@gmail.com>
2026-02-25 14:06:40 -0300

    bando de dados contato

--- bancodedados.sql
+++ bancodedados.sql
@@ -8,6 +8,7 @@ email varchar(255) not null unique,
 senha varchar(255) not null 
 );
 
+
 create table msgcontato(
 id_usuario int not null auto_increment primary key,
 nome_contato varchar (100) not null,
