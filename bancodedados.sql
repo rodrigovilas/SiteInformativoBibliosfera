73f82824eba06773c36b2b6eb816b4bef90ba67a
Miguel Vulcanis <migueltrabalhos75@gmail.com>
2026-03-25 15:32:39 -0300

    Refactor database schema and create new tables
    nao terminado

--- bancodedados.sql
+++ bancodedados.sql
@@ -1,31 +1,68 @@
+drop database if exists bibliosfera;
 create database if not exists bibliosfera;
 use bibliosfera;
 
-create table if not exists login(
-id_user int auto_increment primary key,
-usuario varchar(50) not null unique,
-email varchar(255) not null unique,
-senha varchar(255) not null 
-);
+create table if not exists usuario(
+id_usuario int auto_increment primary key,
+nome_usuario varchar (50) unique not null,
+email varchar (100) unique not null,
+senha varchar (200) not null);
+
+create table if not exists autor(
+id_autor int auto_increment primary key,
+nome varchar(100) not null,
+biografia varchar(2000));
 
 create table if not exists livro(
 id_livro int auto_increment primary key,
-nome_livro varchar(200) not null,
-autor_livro varchar(200) not null,
-anopublicacao_livro decimal(4,0) not null,
-nota_livro decimal(2,1) not null,
-notasite_livro decimal(2,1) not null);
+titulo varchar (200) not null,
+descricao varchar(2000),
+capa varchar (200));
 
-create table if not exists msgcontato(
-id_usuario int not null auto_increment primary key,
-nome_contato varchar (100) not null,
-email_contato varchar (200) not null,
-mensagem_contato varchar(200) not null
-);
+create table if not exists livropraautor(
+id_livro int,
+id_autor int,
+primary key (id_livro, id_autor),
+foreign key (id_livro) references livro(id_livro),
+foreign key (id_autor) references autor(id_autor));
 
-create table if not exists comentarioLeitor(
-usuario varchar(50),
-comentario_leitor varchar(200) not null,
-nome_livro_leitor varchar(200) not null,
-foreign key (usuario) references login(usuario)
-);
+create table if not exists genero(
+id_genero int auto_increment primary key,
+nome_genero varchar(100));
+
+create table if not exists generopralivro(
+id_livro int,
+id_genero int,
+primary key (id_livro, id_genero),
+foreign key (id_livro) references livro(id_livro),
+foreign key (id_genero) references genero(id_genero));
+
+create table if not exists categoria(
+id_categoria int auto_increment primary key,
+nome varchar (100) unique not null,
+descricao varchar(500));
+
+create table if not exists resenha(
+id_resenha int auto_increment primary key,
+id_usuario int,
+id_livro int,
+nota decimal(2,1),
+resenha varchar(2000),
+check (nota>=0 and nota<=10),
+foreign key (id_usuario) references usuario(id_usuario),
+foreign key (id_livro) references livro(id_livro));
+
+create table if not exists categoriapralivro(
+id_categoria int,
+id_livro int,
+primary key (id_categoria, id_livro),
+foreign key (id_categoria) references categoria(id_categoria),
+foreign key (id_livro) references livro(id_livro));
+
+create table if not exists listausuario(
+id_usuario int,
+id_livro int,
+progresso enum('Lendo','Pausado','Terminado'),
+primary key (id_usuario, id_livro),
+foreign key (id_usuario) references usuario(id_usuario),
+foreign key (id_livro) references livro(id_livro));
